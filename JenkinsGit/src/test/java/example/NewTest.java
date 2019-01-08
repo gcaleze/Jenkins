81dc8fcaa4ef35eac55339f642a34511fb059b3f
gcaleze <gcaleze@svi.com.ph>
2019-01-08 21:26:00 +0800

    Remove unused libraries

--- JenkinsGit/src/test/java/example/NewTest.java
+++ JenkinsGit/src/test/java/example/NewTest.java
@@ -3,10 +3,7 @@ package example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
