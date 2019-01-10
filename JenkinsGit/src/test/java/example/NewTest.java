f3eb7bf6735654e48f8ee5e1554ef8c4adf8c08e
gcaleze <gcaleze@svi.com.ph>
2019-01-10 18:58:10 +0800

    Chrome

--- JenkinsGit/src/test/java/example/NewTest.java
+++ JenkinsGit/src/test/java/example/NewTest.java
@@ -34,7 +34,7 @@ public class NewTest {
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		driver = new RemoteWebDriver(new URL("http://34.204.52.158:8080/wd/hub"), capability);
 		driver.get("https://www.google.com/");
-		String page = driver.getPageSource();
+		//String page = driver.getPageSource();
 	}
 
 	@Test(priority = 1)
