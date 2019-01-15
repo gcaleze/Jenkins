75f531fe442369b246e2c41e74f87e4861191d3d
gcaleze <gcaleze@svi.com.ph>
2019-01-15 16:18:02 +0800

    123

--- JenkinsGit/src/main/java/mainSample/base.java
+++ JenkinsGit/src/main/java/mainSample/base.java
@@ -5,21 +5,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 
 public class base {
-	String driverPath = "C:\\Selenium\\chromedriver.exe";
-	//String driverPath = "/usr/bin/chromedriver";	
+	String driverPath;
 	public WebDriver driver;
 
 	@BeforeClass
-	public void launchBrowser(){
+	public void launchBrowser() {
+		locateDriver();
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		String OS = System.getProperty("os.name");
 		System.out.println(OS);
 	}
-	
-//	public String checkOS(){
-//		String OS = System.getProperty("os.name");
-//		return OS;
-//	}
+
+	public void locateDriver() {
+		String OS = System.getProperty("os.name");
+		if (OS.equals("Windows 7")) {
+			driverPath = "C:\\Selenium\\chromedriver.exe";
+		} else {
+			driverPath = "/usr/bin/chromedriver";
+		}
+	}
 }
